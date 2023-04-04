06e0e1b89a03f36f4db8669cc8ed0883a3287682
MrGoc <mrgoc@live.com>
2023-04-04 08:48:16 +0200

    feat: Added api/fileanalyse

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+let express = require("express");
+let cors = require("cors");
+let multer = require("multer");
+let upload = multer({ dest: "uploads/" });
+require("dotenv").config();
 
-var app = express();
+let app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  // req.file is the name of your file in the form above, here 'uploaded_file'
+  // req.body will hold the text fields, if there were any
+  //console.log(req.file, req.body)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
 });
